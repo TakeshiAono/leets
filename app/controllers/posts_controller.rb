059bf3d774bbc0766b6bcad094fabc62e32052bb
aono <takeshi.aono001@gmail.com>
2022-05-22 15:49:19 +0900

    edit 文字数エラーの対応

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,26 @@
 class PostsController < ApplicationController
-  # before_action :set_post, only: %i[ show edit update destroy ]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
     set_post
   end
   
-
-  # GET /posts/new
   def new
     @post = Post.new
-
   end
 
-  # GET /posts/1/edit
   def edit
     set_post    
 
   end
 
-  # POST /posts or /posts.json
   def create
- 
     @post = Post.new(post_params)
     if params[:back]
       render new_post_path
-
     else
         if @post.save
           redirect_to posts_path, notice: "投稿しました"
@@ -38,39 +28,31 @@ class PostsController < ApplicationController
           render new_post_path, notice: "投稿できません"          
         end
     end
-
-    # respond_to do |format|
-    #   if @post.save
-    #     format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-    #     format.json { render :show, status: :created, location: @post }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @post.errors, status: :unprocessable_entity }
-    #   end
-    # end
-
-
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
+    byebug
     set_post
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    # respond_to do |format|
     end
+      if @post.update(post_params)
+        byebug
+        render "show", notice: '更新完了'#edit_post_path(@post[:id])#, 
+        # format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        # format.json { render :show, status: :ok, location: @post }
+      else
+        byebug
+        render "edit", notice: '更新エラー'#edit_post_path(@post[:id])#
+
+        # format.html { render :edit, status: :unprocessable_entity }
+        # format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    # end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     set_post
     @post.destroy
-
     respond_to do |format|
       format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
@@ -82,11 +64,11 @@ class PostsController < ApplicationController
   end
 
   private
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    def post_params
-      params.require(:post).permit(:content)
-    end
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
